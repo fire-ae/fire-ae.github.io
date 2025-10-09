9d660ec0b9554d19fc0c6989a4c6866613be1a53
FIRE Araştırma Eğitim Ltd. Şti <162897064+fire-ae@users.noreply.github.com>
2025-10-09 14:21:46 +0300

    Update fade-in.js

--- fade-in.js
+++ fade-in.js
@@ -1,13 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
     const faders = document.querySelectorAll('.fade-in');
-    const appearOptions = { threshold: 0.1, rootMargin: "0px 0px -50px 0px" };
+    const appearOptions = {
+        threshold: 0.1,
+        rootMargin: "0px 0px -50px 0px"
+    };
 
-    const observer = new IntersectionObserver((entries) => {
+    const appearOnScroll = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) entry.target.classList.add("appear");
             else entry.target.classList.remove("appear");
         });
     }, appearOptions);
 
-    faders.forEach(fader => observer.observe(fader));
+    faders.forEach(fader => appearOnScroll.observe(fader));
 });
